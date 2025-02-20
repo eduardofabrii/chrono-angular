b88aaf291e1a7f865572fcb893e02e6e7ef33888
Eduardo Fabri <eduardohfabri@gmail.com>
2025-02-20 15:22:11 -0300

    Implementando tela de projetos

---
+++ src/app/modules/pages/projects/page/projects-home/projects-home.component.ts
@@ -0,0 +1,52 @@
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+
+import { ProjectsService } from '../../../../../services/projects/projects.service';
+import { GetProjectResponse } from '../../../../../models/interfaces/projects/response/GetProjectResponse';
+
+@Component({
+  selector: 'app-projects-home',
+  templateUrl: './projects-home.component.html',
+  styleUrls: ['./projects-home.component.scss']
+})
+export class ProjectsHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$ = new Subject<void>();
+  public projects: GetProjectResponse[] = [];
+  public filteredProjects: GetProjectResponse[] = [];
+  public selectedProject!: GetProjectResponse;
+  public isVisibleShowMoreDialog: boolean = false;
+
+  public name: string = '';
+
+  projectsService = inject(ProjectsService);
+
+  ngOnInit(): void {
+    this.getProjects();
+  }
+
+  private getProjects(): void {
+    this.projectsService.getAllProjects()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((projects) => {
+        this.projects = projects;
+        this.filteredProjects = projects;
+      }
+    );
+  }
+
+  public openShowMore(project: GetProjectResponse): void {
+    this.selectedProject = project;
+    this.isVisibleShowMoreDialog = true;
+  }
+
+  public onCloseDialog(dialogType: "showMore"): void {
+    if (dialogType == "showMore") {
+      this.isVisibleShowMoreDialog = false;
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
